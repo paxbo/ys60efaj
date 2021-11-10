0d674569bf262dd0f65c51697f71aa2377d93bff
paxbo <paul.borutta@st.ovgu.de>
2021-11-10 13:02:30 +0100

    change in function

--- ys60efaj/function.py
+++ ys60efaj/function.py
@@ -3,18 +3,17 @@ from ipywidgets import interact, fixed
 from PIL import Image
 from matplotlib import cm
 
-def imshow(X, width, height):
+
+def imshow(X,width, height):
     """
     You should create a way to resize an image from an array X.
     The use of widgets is optional but you can take a look to interact.
     We should be able to install this package in Google Colab from your Git
     repo.
     """
-    myImage.close()
-    X=np.uint8(cm.gist_earth(myarray)*255)
     myImage = Image.fromarray(X)
-    myImage.reszize((width,height))
-    Image.open(myImage)
-
-
-interact(imshow, width=(1,5,2000), height=(1, 5, 2000));
+    myNewImage = myImage.resize((width,height))
+    print("Image size: {}".format(myNewImage.size))
+    myNewImage.show()
+    
+interact(imshow,X=fixed(np.random.rand(100,100)), width=(1,2000,5), height=(1, 2000, 5));
